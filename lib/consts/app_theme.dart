5dab0858aa9658f8971edffc746ce7a8ca5829dd
Tasneem.Ibrahim <tasneemadeh98@outlook.com>
2025-08-05 20:55:54 +0300

    Modify the app theme

--- lib/consts/app_theme.dart
+++ lib/consts/app_theme.dart
@@ -2,33 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
-  static Color grey800 = Colors.grey.shade800;
-  static Color grey200 = Colors.grey.shade200;
+  static Color kGrey800 = Colors.grey.shade800;
+  static Color kGrey200 = Colors.grey.shade200;
 
-  static const Color secondaryColor = Color(0xFF1E1E1E);
+  static Color kBlack54 = Colors.black54;
 
-  static Color black54 = Colors.black54;
+  static RoundedRectangleBorder kRoundRectangleBorder = RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(12.0),
+  );
 
   static ThemeData themeData(double screenWidth) => ThemeData(
     colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey.shade800),
     fontFamily: GoogleFonts.cairo().fontFamily,
+    scaffoldBackgroundColor: Colors.white70,
     cardTheme: CardThemeData(
       elevation: 5.0,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
-      color: grey200,
+      shape: kRoundRectangleBorder,
+      color: kGrey200,
     ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         fixedSize: Size(screenWidth * 0.8, 50.0),
-        backgroundColor: black54,
+        backgroundColor: kBlack54,
         foregroundColor: Colors.white,
         textStyle: GoogleFonts.cairo(
           fontSize: 20.0,
           fontWeight: FontWeight.bold,
         ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12.0),
-        ),
+        shape: kRoundRectangleBorder,
       ),
     ),
   );

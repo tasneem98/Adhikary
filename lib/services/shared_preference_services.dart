5f5d182e01d3862aaa8f7e8abf575f0247eeb7a0
Tasneem.Ibrahim <tasneemadeh98@outlook.com>
2025-08-28 04:09:24 +0300

    ay 7aga

--- lib/services/shared_preference_services.dart
+++ lib/services/shared_preference_services.dart
@@ -3,11 +3,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SharedPreferenceServices {
   // Keys
   static const String _enableNotificationKey = 'enable_notification_key';
-  static const String _enableOverLayWindowKey = 'enable_overlay_window_key';
 
   static String get enableNotification => _enableNotificationKey;
-
-  static String get enableOverLayWindow => _enableOverLayWindowKey;
 
   // Shared Preference Singleton
   static final SharedPreferenceServices _instance =
